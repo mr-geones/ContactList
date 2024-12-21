7f9b85cf6cbedf4db580fd009f08e8ed43bbf93a
GEO <geones@live.se>
2024-12-21 20:37:17 +0100

    Add new projects and refactor solution structure
    Updated ContactList.sln to include ConsoleApp_Main and Business projects. Removed ContactList.csproj. Added Business.csproj targeting .NET 9.0 with implicit usings and nullable reference types, including Services folder. Introduced ContactFactory class in Business.Factories namespace. Added IContactService and IFileService interfaces in Business.Interfaces namespace. Created Contact class in Business.Models namespace with basic properties. Added ConsoleApp_Main.csproj targeting .NET 9.0, referencing Business project, and including Dialogs folder.

---
+++ Business/Interfaces/IContactService.cs
@@ -0,0 +1,15 @@
+﻿using Business.Models;
+
+namespace Business.Interfaces;
+
+public interface IContactService
+{
+    // För godkänd
+    bool CreateContact(Contact contact);
+    IEnumerable<Contact> GetAllContacts();
+
+    // För väl godkänd
+    Contact GetContactById(Func<Contact, bool> predicate);
+    bool UpdateContact(Func<Contact, bool> predicate, Contact updatedContact);
+    bool DeleteContact(Func<Contact, bool> predicate);
+}
---
+++ Business/Models/Contact.cs
@@ -0,0 +1,10 @@
+﻿namespace Business.Models;
+
+public class Contact
+{
+    public string Id { get; set; } = null!;
+    public string Forename { get; set; } = null!;
+    // public string Surname { get; set; } = null!;
+    // public string Phone { get; set; }
+    // public string Message { get; set; }
+}
